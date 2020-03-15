4b6136123b17ff0e6f18cbb2cb3fc7cf4098fc0b
SerhiiCho <serhiicho@protonmail.com>
2020-03-15 10:12:48 +0200

    Formatter code

--- PDO/index.php
+++ PDO/index.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 $pdo = new PDO('mysql:host=localhost;dbname=pdo', 'serhii', '111111');
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
--- Sqlite/functions.php
+++ Sqlite/functions.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 function getAllFrom(string $table_name, SQLite3 $db_inst): array
 {
