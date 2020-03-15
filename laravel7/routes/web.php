e429a39168243b8c26c1068f27be0044648be128
SerhiiCho <serhiicho@protonmail.com>
2020-03-15 17:03:39 +0200

    laravel make auth

--- laravel7/routes/web.php
+++ laravel7/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+function of(string $str): Stringable {
+    return Str::of($str);
+}
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'var' => of('nice')->upper(),
+    ]);
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
