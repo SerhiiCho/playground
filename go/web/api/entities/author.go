06553a51b189a088ab36c4eefd59315a9ac7d105
SerhiiCho <serhiicho@protonmail.com>
2020-03-26 20:18:48 +0200

    Added 2 entities

---
+++ go/web/api/entities/author.go
@@ -0,0 +1,7 @@
+package entities
+
+// Author model
+type Author struct {
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
+}
---
+++ go/web/api/entities/book.go
@@ -0,0 +1,9 @@
+package entities
+
+// Book model
+type Book struct {
+	ID     string  `json:"id,omitempty"`
+	Isbn   string  `json:"isbn,omitempty"`
+	Title  string  `json:"title,omitempty"`
+	Author *Author `json:"author,omitempty"`
+}
