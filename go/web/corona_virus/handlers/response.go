8ad13cde8744a95ff59a03fb048154a4867e65bf
SerhiiCho <serhiicho@protonmail.com>
2020-03-26 19:20:49 +0200

    Refactored response.go

--- go/web/corona_virus/handlers/response.go
+++ go/web/corona_virus/handlers/response.go
@@ -1,20 +1,26 @@
 package handlers
 
-// ResponseItem from API endpoint
-type ResponseItem struct {
-	Country string `json:"country"`
-	Cases   struct {
-		New       string `json:"new"`
-		Total     int    `json:"total"`
-		Recovered int    `json:"recovered"`
-	} `json:"cases"`
-	Deaths struct {
-		New   string `json:"new"`
-		Total int    `json:"total"`
-	} `json:"deaths"`
+// ResponseCases child of ResponseItem
+type ResponseCases struct {
+	New       string `json:"new"`
+	Total     int    `json:"total"`
+	Recovered int    `json:"recovered"`
 }
 
-// Response from API endpoint
+// ResponseDeaths child of ResponseItem
+type ResponseDeaths struct {
+	New   string `json:"new"`
+	Total int    `json:"total"`
+}
+
+// ResponseItem child of Response
+type ResponseItem struct {
+	Country string         `json:"country"`
+	Cases   ResponseCases  `json:"cases"`
+	Deaths  ResponseDeaths `json:"deaths"`
+}
+
+// Response representation of json from API endpoint
 type Response struct {
 	Get        string         `json:"get"`
 	Parameters []interface{}  `json:"parameters"`
