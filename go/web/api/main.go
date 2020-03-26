1a0f2f77e5de5582d8c6cd6092571977ea77bb67
SerhiiCho <serhiicho@protonmail.com>
2020-03-26 20:58:31 +0200

    Moved code from main.go file

--- go/web/api/main.go
+++ go/web/api/main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 
-	"database/sql"
+	"./server"
 
-	"./handlers"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "serhii:111111@/api")
-
-	router := mux.NewRouter()
-
-	router.HandleFunc("/api/books", handlers.GetBooks).Methods("GET")
-	router.HandleFunc("/api/books/{id}", handlers.GetBook).Methods("GET")
-	router.HandleFunc("/api/books", handlers.CreateBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", handlers.DeleteBook).Methods("DELETE")
-
-	fmt.Println("Serving on http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	if err := server.Start(); err != nil {
+		log.Fatalf("Starting server error. Message %s", err)
+	}
 }
