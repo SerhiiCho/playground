f9ac99aa9ea5e9a29a142b4e622873cb0fc6a5ff
SerhiiCho <serhiicho@protonmail.com>
2020-03-26 19:23:37 +0200

    Changes in response.go

--- go/web/corona_virus/handlers/response.go
+++ go/web/corona_virus/handlers/response.go
@@ -1,5 +1,11 @@
 package handlers
 
+// Response representation of json from API endpoint
+type Response struct {
+	Results  int            `json:"results"`
+	Response []ResponseItem `json:"response"`
+}
+
 // ResponseCases child of ResponseItem
 type ResponseCases struct {
 	New       string `json:"new"`
@@ -18,10 +24,4 @@ type ResponseItem struct {
 	Country string         `json:"country"`
 	Cases   ResponseCases  `json:"cases"`
 	Deaths  ResponseDeaths `json:"deaths"`
-}
-
-// Response representation of json from API endpoint
-type Response struct {
-	Results  int            `json:"results"`
-	Response []ResponseItem `json:"response"`
 }
