945ed0ee5bdc650e22b2f4744410e25ec5099050
SerhiiCho <serhiicho@protonmail.com>
2020-03-26 13:03:27 +0200

    Changed handler.go in coronavirus go example

--- go/web/corona_virus/handlers/handler.go
+++ go/web/corona_virus/handlers/handler.go
@@ -1,10 +1,13 @@
 package handlers
 
-import "html/template"
+import (
+	"html/template"
+	"strconv"
+)
 
 const (
-	url  = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php"
-	host = "coronavirus-monitor.p.rapidapi.com"
+	url  = "https://covid-193.p.rapidapi.com/statistics"
+	host = "covid-193.p.rapidapi.com"
 	key  = "091b42d8ecmsh380ec626aa86af5p14908ajsne8ee54be4633"
 )
 
@@ -14,18 +17,43 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*gohtml"))
 }
 
+type ResponseItem struct {
+	Country string `json:"country"`
+	Cases   struct {
+		New       string `json:"new"`
+		Total     int    `json:"total"`
+		Recovered int    `json:"recovered"`
+	} `json:"cases"`
+	Deaths struct {
+		New   string `json:"new"`
+		Total int    `json:"total"`
+	} `json:"deaths"`
+}
+
 // Response from API endpoint
 type Response struct {
-	AffectedCountries []string `json:"affected_countries"`
-	StatisticTakenAt  string   `json:"statistic_taken_at"`
+	Get        string         `json:"get"`
+	Parameters []interface{}  `json:"parameters"`
+	Errors     []interface{}  `json:"errors"`
+	Results    int            `json:"results"`
+	Response   []ResponseItem `json:"response"`
 }
 
 // ToHTML returns list of countries with html tags
 func (r *Response) ToHTML() string {
-	list := "<ul class=\"list-group\">"
+	title := "<h3>" + strconv.Itoa(len(r.Response)) + " countires</h3>"
+	list := title + `<ul class="list-group">`
 
-	for _, country := range r.AffectedCountries {
-		list += "<li class=\"list-group-item\">" + country + "</li>"
+	for _, item := range r.Response {
+		list += `
+			<li class="list-group-item">
+				<b>` + item.Country + `</b>
+				<br>Cases: ` + strconv.Itoa(item.Cases.Total) + `
+				<br>New cases: ` + item.Cases.New + `
+				<br>Deaths: ` + strconv.Itoa(item.Deaths.Total) + `
+				<br>Recovered: ` + strconv.Itoa(item.Cases.Recovered) + `
+			</li>
+		`
 	}
 
 	return list + "</ul>"
