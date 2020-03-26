3b392e226bd1e587e155c962bf0ec7e1d5aa21ec
SerhiiCho <serhiicho@protonmail.com>
2020-03-26 20:30:35 +0200

    Added ID fielid to struct in author.go

--- go/web/api/entities/author.go
+++ go/web/api/entities/author.go
@@ -2,6 +2,7 @@ package entities
 
 // Author model
 type Author struct {
+	ID        int    `json:"id,omitempty"`
 	FirstName string `json:"first_name,omitempty"`
 	LastName  string `json:"last_name,omitempty"`
 }
--- go/web/api/entities/book.go
+++ go/web/api/entities/book.go
@@ -2,7 +2,7 @@ package entities
 
 // Book model
 type Book struct {
-	ID     string  `json:"id,omitempty"`
+	ID     int     `json:"id,omitempty"`
 	Isbn   string  `json:"isbn,omitempty"`
 	Title  string  `json:"title,omitempty"`
 	Author *Author `json:"author,omitempty"`
