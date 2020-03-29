a1dcc5c81956e8a970c2345e9d5495ed0d6ca644
SerhiiCho <serhiicho@protonmail.com>
2020-03-29 15:54:33 +0300

    Removed map fron createBook method in create_book.go file

--- go/web/api/server/create_book.go
+++ go/web/api/server/create_book.go
@@ -7,9 +7,7 @@ import (
 
 func (s *server) createBook() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := tpl.ExecuteTemplate(w, "create_book.gohtml", map[string]interface{}{
-			"Page": "create_book",
-		})
+		err := tpl.ExecuteTemplate(w, "create_book.gohtml", nil)
 		log.Printf("Executing template error. Message: %s", err)
 	}
 }
