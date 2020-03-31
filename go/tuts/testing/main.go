8d79f0d08bdce549736045e9e30f32562f22717f
SerhiiCho <serhiicho@protonmail.com>
2020-03-31 14:56:21 +0300

    Added 1 more go lang example

---
+++ go/tuts/testing/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+func calc(num1, num2 int) (result int) {
+	result = num1 + num2
+	return result
+}
---
+++ go/tuts/testing/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func Test_calc(t *testing.T) {
+	type args struct {
+		num1 int
+		num2 int
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantResult int
+	}{
+		{
+			name:       "simple numbers",
+			args:       args{num1: 1, num2: 1},
+			wantResult: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotResult := calc(tt.args.num1, tt.args.num2); gotResult != tt.wantResult {
+				t.Errorf("calc() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
