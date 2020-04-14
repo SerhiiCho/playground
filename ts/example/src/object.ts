437c4c044e2d54ff1accca57518fb9f96273406d
SerhiiCho <serhiicho@protonmail.com>
2020-04-14 21:09:12 +0300

    Changed typescript example files

--- ts/example/src/object.ts
+++ ts/example/src/object.ts
@@ -1,7 +1,15 @@
-const person: {
+interface Person {
     name: string
     age: number
-} = {
-    name: 'Mikel',
-    age: 25
+    getInfo: Function,
 }
+
+const person: Person = {
+    name: 'Mikel',
+    age: 25,
+    getInfo: (): string => {
+        return `The name is ${person.name} and age is ${person.age}`
+    },
+}
+
+export default person
