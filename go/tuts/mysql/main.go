4f9d421378b7f3fd6466427f1395808e181d2c2b
SerhiiCho <serhiicho@protonmail.com>
2020-04-18 20:23:18 +0300

    Working on sql in go

--- go/tuts/mysql/main.go
+++ go/tuts/mysql/main.go
@@ -13,6 +13,62 @@ const (
 	sqlDataSourceName = "serhii:111111@tcp(localhost:3306)/mysql_test?charset=utf8"
 )
 
+func main() {
+	db, err := sql.Open(sqlDriverName, sqlDataSourceName)
+	PrintOnError(err)
+
+	insertUser(db, User{
+		Name: "Mikel",
+		Age:  32,
+	})
+
+	printAll(db)
+}
+
+func insertUser(db *sql.DB, user User) {
+	sql := `
+		insert into users (name, age)
+		values (?, ?)
+	`
+
+	res, err := db.Exec(sql, user.Name, user.Age)
+	PrintOnError(err)
+
+	id, err := res.LastInsertId()
+	PrintOnError(err)
+
+	fmt.Printf("User with id %d has been created\n", id)
+}
+
+func printAll(db *sql.DB) {
+	rows, err := db.Query("select name, age from users")
+	PrintOnError(err)
+
+	var users []User
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var name sql.NullString
+		var age sql.NullInt64
+
+		rows.Scan(&name, &age)
+
+		if !name.Valid || !age.Valid {
+			fmt.Println("Name or Age is not valid")
+		}
+
+		users = append(users, User{
+			Name: name.String,
+			Age:  int(age.Int64),
+		})
+	}
+
+	for _, user := range users {
+		fmt.Printf("My name is %s, and I'm %d years old\n", user.Name, user.Age)
+	}
+}
+
 func PrintOnError(err interface{}) {
 	if err != nil {
 		log.Fatalln(err)
@@ -22,24 +78,4 @@ func PrintOnError(err interface{}) {
 type User struct {
 	Name string
 	Age  int
-}
-
-func main() {
-	db, err := sql.Open(sqlDriverName, sqlDataSourceName)
-	PrintOnError(err)
-
-	rows, err := db.Query("select name, age from users")
-	PrintOnError(err)
-
-	var users []User
-
-	for rows.Next() {
-		var user User
-		rows.Scan(&user.Name, &user.Age)
-		users = append(users, user)
-	}
-
-	for _, user := range users {
-		fmt.Printf("My name is %s, and I'm %d years old\n", user.Name, user.Age)
-	}
 }
