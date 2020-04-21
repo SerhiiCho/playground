49803249b276a5f57702a4f842e2b53af9b7ab7b
SerhiiCho <serhiicho@protonmail.com>
2020-04-21 18:05:21 +0300

    Removed comment fron construct in HomeController in laravel example

--- php/laravel7_crud/app/Http/Controllers/HomeController.php
+++ php/laravel7_crud/app/Http/Controllers/HomeController.php
@@ -7,11 +7,6 @@ use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
