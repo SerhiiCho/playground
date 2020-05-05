ffa71e5354e4baf0f89ec42ba969a36a7703ecb8
serhii <serhiicho@protonmail.com>
2020-05-05 15:55:29 +0300

    Working on key value store in golang

--- go/key_value_store/main.go
+++ go/key_value_store/main.go
@@ -6,38 +6,27 @@ import (
 	"os"
 )
 
-var (
-	command string
-	key     string
-	value   string
-)
-
 func main() {
-	if len(os.Args) != 3 || len(os.Args) != 4 {
+	if len(os.Args) != 3 && len(os.Args) != 4 {
 		log.Fatalf("There must be 3 or 4 arguments instead of %d\n", len(os.Args))
 	}
 
-	command = os.Args[1]
+	command := os.Args[1]
 
 	switch command {
 	case "set":
-		handleSetCommand()
+		handleSetCommand(os.Args[2], os.Args[3])
 	case "get":
-		handleGetCommand()
+		handleGetCommand(os.Args[4])
 	default:
 		log.Fatalln("Command must be `set` or `get`")
 	}
-
-	key = os.Args[2]
-	value = os.Args[3]
-
-	fmt.Println()
 }
 
-func handleSetCommand() {
-
+func handleSetCommand(key, value string) {
+	fmt.Println(key, value)
 }
 
-func handleGetCommand() {
-
+func handleGetCommand(key string) {
+	fmt.Println(key)
 }
