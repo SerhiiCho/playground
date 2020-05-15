48427d20370b3531a993250d9f471b52159eff43
serhii <serhiicho@protonmail.com>
2020-05-15 12:24:36 +0300

    Added nodes project

---
+++ php/nodes/app/Tree.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class Tree
+{
+    public function __construct()
+    {
+        #
+    }
+}
---
+++ php/nodes/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nodes tree</title>
+    <link rel="stylesheet" href="assets/materialize.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="nodes">
+            <div class="node">
+                <span><b>1</b></span>
+
+                <div class="children">
+                    <div class="node">
+                        <span><b>2</b></span>
+
+                        <div class="children">
+                            <div class="node">
+                                <span><b>4</b></span>
+                            </div>
+                            <div class="node">
+                                <span><b>5</b></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="node">
+                        <span><b>3</b></span>
+
+                        <div class="children">
+                            <div class="node">
+                                <span><b>6</b></span>
+                            </div>
+                            <div class="node">
+                                <span><b>7</b></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/main.js"></script>
+</body>
+</html>
