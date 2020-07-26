fd237d5442830648d8f2f9f30ade379a389ac86e
serhii <serhiicho@protonmail.com>
2020-07-26 18:13:21 +0300

    Changed main.go in corona_virus example

--- go/web/corona_virus/main.go
+++ go/web/corona_virus/main.go
@@ -8,11 +8,13 @@ import (
 	"./handlers"
 )
 
+const port = ":8080"
+
 func main() {
 	http.HandleFunc("/", handlers.Home)
 
-	fmt.Println("Listening on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Listening on http://localhost" + port)
+	err := http.ListenAndServe(port, nil)
 
 	log.Fatal("Listen and serve error:", err)
 }
