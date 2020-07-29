bc500a93ca6b8d4113dbbd7c4b381bd588663ec3
serhii <serhiicho@protonmail.com>
2020-07-29 22:01:53 +0300

    Changes to go lang files

--- go/web/crud/server/create_book.go
+++ go/web/crud/server/create_book.go
@@ -14,6 +14,6 @@ func (s *server) createBook() http.HandlerFunc {
 		}
 
 		err := tpl.ExecuteTemplate(w, "create_book.html", authors)
-		log.Printf("Executing template error. Message: %s", err)
+		log.Println("Executing template error.", err)
 	}
 }
--- go/web/router/main.go
+++ go/web/router/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/SerhiiCho/go-playground/web/router/handlers"
+	"./handlers"
 )
 
 const (
