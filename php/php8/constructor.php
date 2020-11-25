bae9ea720f038278a23b488781b27a74f6e862a2
serhii <serhiicho@protonmail.com>
2020-11-25 21:35:44 +0200

    Added example for contructor promotion in php8

---
+++ php/php8/constructor.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+class Name
+{
+    public function __construct(private string $name){
+        echo $name;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
+
+class User
+{
+    public function __construct(public Stringable $name) {}
+}
+
+// Stringable and getting the name of the class
+$user = new User(new Name('Anna'));
+$class_name = $user::class;
+
+echo $user->name;
--- php/php8/features.php
+++ php/php8/features.php
@@ -7,28 +7,6 @@ $string_starts = str_starts_with($string, 'Some');
 $string_ends = str_ends_with($string, 'here');
 $var_type = get_debug_type($string);
 
-// class Name with trailing comma after $name param
-class Name
-{
-    public function __construct(
-        private string $name,
-    ){}
-
-    public function __toString(): string
-    {
-        return $this->name;
-    }
-}
-
-class User
-{
-    public function __construct(public Stringable $name) {}
-}
-
-// Stringable and getting the name of the class
-$user = new User(new Name('Anna'));
-$class_name = $user::class;
-
 // Match
 $name = 'Anna';
 $her_name = match($name) {
