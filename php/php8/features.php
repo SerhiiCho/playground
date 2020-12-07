4aa7f27dcfe02cc64425ff593a8e69f1bc4c5e34
serhii <serhiicho@protonmail.com>
2020-12-07 19:44:38 +0200

    Changed features.php

--- php/php8/features.php
+++ php/php8/features.php
@@ -1,13 +1,13 @@
 <?php
 
-$string = 'Some string is here';
+$string = 'Hello world';
 
 $string_contains = str_contains($string, 'string');
 $string_starts = str_starts_with($string, 'Some');
 $string_ends = str_ends_with($string, 'here');
 $var_type = get_debug_type($string);
 
-// Match
+// Match block
 $name = 'Anna';
 $her_name = match($name) {
     'Anna' => 'This is Anna',
@@ -15,7 +15,7 @@ $her_name = match($name) {
     default => throw new Exception('Doesnt match any names'),
 };
 
-// Named
+// Named arguments
 $html_chars = htmlspecialchars('Some string', double_encode: false);
 
 var_dump($html_chars);
