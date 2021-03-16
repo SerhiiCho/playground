18b75f3b48493f0b135415272269315be886ab37
serhii <serhiicho@protonmail.com>
2021-03-16 15:40:34 +0200

    Playing with go

---
+++ go/web/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	articles := []Article{
+		{
+			Title:       "How to make your life nicer",
+			Description: "This is a small description",
+			Content:     "Content is here",
+		},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(articles)
+}
