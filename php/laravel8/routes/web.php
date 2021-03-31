20f213c773935bdb31ff863dfce1da31217ea2bd
serhii <serhiicho@protonmail.com>
2021-03-31 18:23:14 +0300

    Work on laravel8 example

--- php/laravel8/routes/web.php
+++ php/laravel8/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with('posts', Post::orderBy('created_at', 'DESC')->get());
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
