bb1bd08b0b08d57559d978f7764ab973034f256e
SerhiiCho <serhiicho@protonmail.com>
2021-04-22 14:38:56 +0300

    Working on todo app with react

--- ts/react-first-try/src/App.tsx
+++ ts/react-first-try/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Form from './components/Form'
+import TodoList from './components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <header>
+                <h1>Serhii's Todo List</h1>
+            </header>
+            <Form />
+            <TodoList />
+        </div>
+    )
 }
-
-export default App;
---
+++ ts/react-first-try/src/components/TodoList.tsx
@@ -0,0 +1,7 @@
+export default function TodoList() {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list"></ul>
+        </div>
+    )
+}
