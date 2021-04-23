3671e75e185241cab1ec049cc7483e7f8cb421d8
SerhiiCho <serhiicho@protonmail.com>
2021-04-23 14:30:42 +0300

    Finished form vue example

--- vue/forms/src/App.vue
+++ vue/forms/src/App.vue
@@ -1,8 +1,31 @@
 <template>
-
+    <div class="container">
+        <textarea v-model="text"></textarea>
+        <h3>{{ text }}</h3>
+    </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
+    setup() {
+        const text = ref('')
+        return { text }
+    }
 }
 </script>
+
+<style scoped>
+.container {
+    padding: 40px;
+}
+textarea {
+    padding: 10px;
+    border-radius: 4px;
+    border: 1px solid #999;
+    width: 500px;
+    height: 100px;
+    font-size: 1.5em;
+}
+</style>
