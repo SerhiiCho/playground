8866b38fdb1730a79ccc992a297903fe9c1ec6cf
SerhiiCho <serhiicho@protonmail.com>
2021-10-08 21:14:07 +0300

    Other changes

---
+++ vue/vue-pdf/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <VuePdf src="https://www.orimi.com/pdf-test.pdf"></VuePdf>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+const VuePdf = require('vue-pdf')
+
+export default defineComponent({
+    name: 'App',
+
+    created() {
+        //
+    },
+
+    components: {
+        HelloWorld,
+        VuePdf,
+    },
+})
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
