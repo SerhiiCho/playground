d0b6ae1e7779fa697197c02f63f4cc717a78fe0c
SerhiiCho <serhiicho@protonmail.com>
2021-10-11 17:48:32 +0300

    Added garden size section

--- html/house-report-template/test.js
+++ html/house-report-template/test.js
@@ -5,11 +5,13 @@ const html = fs.readFileSync('./index.html', { encoding: 'utf-8' })
 const css = fs.readFileSync('./style.css', { encoding: 'utf-8' })
 
 axios.post('https://hub.nalog.nl/api/v1/generators/pdf-file', {
-    'token': 'KyJzdkIiOiIxMjM0NTYJODkwIiNibmFtzSI3IkpvaljM8MiDsfQ',
-    'content': html,
-    'css': css,
-    "is_public": true,
-    "dir_name": 'house-pdf-report',
+    token: 'KyJzdkIiOiIxMjM0NTYJODkwIiNibmFtzSI3IkpvaljM8MiDsfQ',
+    content: html,
+    css: css,
+    is_public: true,
+    dir_name: 'house-pdf-report',
+    margin_top: 10,
+    margin_bottom: 10,
 })
     .then(res => console.log(res.data))
     .catch(err => console.error(err))
