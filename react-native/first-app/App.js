fc379e4017b7719ab0c20460adf43911e3a7bc8e
SerhiiCho <serhiicho@protonmail.com>
2022-05-31 08:56:45 +0300

    Added more styles to react first app

--- react-native/first-app/App.js
+++ react-native/first-app/App.js
@@ -1,22 +1,38 @@
-import React, {useState} from 'react'
-import {Button, StyleSheet, Text, View} from 'react-native'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native'
 
 export default function App() {
     const [outputText, setOutputText] = useState('Before click')
 
     return (
-        <View style={styles.container}>
-            <Text>{outputText}</Text>
-            <Button title="Click me" onPress={() => setOutputText('After click')} />
+        <View style={{ padding: 50 }}>
+            <View style={{ display: 'flex' }}>
+                <Text style={styles.title}>My notes</Text>
+
+                <TextInput
+                    placeholder='Write a note'
+                    style={styles.input}
+                />
+                <Button title="Add a note"
+                    onPress={() => setOutputText('After click')}
+                />
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+const styles = {
+    input: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 10,
+        marginTop: 20,
+        borderRadius: 7,
     },
-})
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        marginTop: 10,
+    },
+}
