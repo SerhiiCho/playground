469e6de95cffe6043061b8db7ef1588fb836ec88
SerhiiCho <serhiicho@protonmail.com>
2022-06-02 16:35:59 +0300

    Added webview example

--- react-native/webview/App.js
+++ react-native/webview/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <WebView source={{ uri: 'https://reactnative.dev/' }} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        width: '100%',
+        height: '100%',
+    },
+})
