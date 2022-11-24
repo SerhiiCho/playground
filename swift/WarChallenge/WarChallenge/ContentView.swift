bcde1bc874bf12e15d3b85bc5b66e0eab5648bdf
SerhiiCho <serhiicho@protonmail.com>
2022-11-24 14:23:35 +0200

    WIP on war challenge

--- swift/WarChallenge/WarChallenge/ContentView.swift
+++ swift/WarChallenge/WarChallenge/ContentView.swift
@@ -1,6 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerCard = "card7"
+    @State private var cpuCard = "card5"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         return ZStack {
             Image("background")
@@ -12,15 +17,16 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                // Cards
+
                 HStack {
                     Spacer()
                     
-                    Image("card3")
+                    Image(playerCard)
                         .cornerRadius(4.0)
+                    
                     Spacer()
                     
-                    Image("card4")
+                    Image(cpuCard)
                         .cornerRadius(4.0)
                     
                     Spacer()
@@ -29,10 +35,12 @@ struct ContentView: View {
                 Spacer()
                 
                 // Deal button
-                Image("dealbutton")
-                    .padding(1)
-                    .border(Color.red)
-                    .cornerRadius(4.0)
+                Button(action: handleDealButtonClick, label: {
+                    Image("dealbutton")
+                        .padding(1)
+                        .border(Color.red)
+                        .cornerRadius(4.0)
+                })
                 
                 Spacer()
                 
@@ -40,36 +48,51 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     
-                    VStack {
-                        Text("Player")
-                            .font(.headline)
-                            
-                            .padding(.bottom, 1.0)
-                        
-                        
-                        Text("0")
-                            .font(.largeTitle)
-                            .foregroundColor(Color.white)
-                    }
+                    getPlayerSection(player: "Player", score: playerScore)
                     
                     Spacer()
                     
-                    VStack {
-                        Text("CPU")
-                            .font(.headline)
-                            .foregroundColor(Color.white)
-                            .padding(.bottom, 1.0)
-                        
-                        Text("0")
-                            .font(.largeTitle)
-                            .foregroundColor(Color.white)
-                    }
+                    getPlayerSection(player: "CPU", score: cpuScore)
                     
                     Spacer()
                 }
                 
                 Spacer()
             }
+        }
+    }
+    
+    func getPlayerSection(player: String, score: Int) -> some View {
+        VStack {
+            Text(player)
+                .font(.headline)
+                .foregroundColor(Color.white)
+                .padding(.bottom, 1.0)
+            
+            Text(String(score))
+                .font(.largeTitle)
+                .foregroundColor(Color.white)
+        }
+    }
+    
+    func handleDealButtonClick() -> Void {
+        let range = 2...14
+        let playerRand = Int.random(in: range)
+        let cpuRand = Int.random(in: range)
+        
+        playerCard = "card" + String(playerRand)
+        cpuCard = "card" + String(cpuRand)
+        
+        if playerRand > cpuRand {
+            playerScore += 1
+        }
+        
+        if cpuRand > playerRand {
+            cpuScore += 1
+        }
+        
+        if cpuRand == playerRand {
+            
         }
     }
 }
