d150cf578be2cea9bc17bc180f7be13bef90891f
SerhiiCho <serhiicho@protonmail.com>
2022-12-11 20:57:53 +0200

    smooth loader

--- vue/vite/src/main.ts
+++ vue/vite/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import SmoothLoaderImg from 'smooth-loader-vue/src'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(SmoothLoaderImg)
+    .mount('#app')
