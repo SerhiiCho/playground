8d02acf5b58193332f8717dd6ce89f95c5ef22c2
SerhiiCho <serhiicho@protonmail.com>
2022-12-17 09:09:11 +0200

    WIP on intersection observer example

--- js/intersection-observer/index.js
+++ js/intersection-observer/index.js
@@ -1,31 +1,12 @@
-const observer = new IntersectionObserver(function (entries, observer) {
+const target = document.getElementById('target')
+
+const observer = new IntersectionObserver()
+observer.observe(target)
+
+function observerHandler(entries, observer) {
     for (const entry of entries) {
         if (entry.intersectionRatio > 0) {
-            handleAppear(entry.target)
-        }
-
-        if (entry.intersectionRatio <= 0) {
-            handleDisappear(entry.target)
+            console.log('appear')
         }
     }
-})
-
-/**
- * @param {Element} elem
- */
-function handleAppear(elem) {
-    console.log('appear')
-    elem.style.opacity = '1'
 }
-
-/**
- * @param {Element} elem
- */
-function handleDisappear(elem) {
-    console.log('disappear')
-    elem.style.opacity = '0'
-}
-
-const elem = document.getElementById('target')
-
-observer.observe(elem)
