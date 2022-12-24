d497ae4b94c9de1317f78c49466122e58dec74ba
SerhiiCho <serhiicho@protonmail.com>
2022-12-24 10:54:55 +0200

    WIP with playground

--- js/intersection-observer/index.js
+++ js/intersection-observer/index.js
@@ -1,13 +1,13 @@
+const observer = new IntersectionObserver(observerHandler)
+
+function observerHandler(entries) {
+    for (const entry of entries) {
+        if (entry.intersectionRatio > 0) {
+            entry.target.src = entry.target.dataset.src
+        }
+    }
+}
+
 const images = document.querySelectorAll('section img')
 
-images.forEach(img => {
-    const observer = new IntersectionObserver((entries) => {
-        for (const entry of entries) {
-            if (entry.intersectionRatio > 0) {
-                img.src = img.dataset.src
-            }
-        }
-    })
-
-    observer.observe(img)
-})
+images.forEach(img => observer.observe(img))
