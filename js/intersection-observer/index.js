1bc2d7bc5331eae4e03c8ba2ec81fec3cdbd13dc
SerhiiCho <serhiicho@protonmail.com>
2022-12-25 12:19:59 +0200

    Changes

--- js/intersection-observer/index.js
+++ js/intersection-observer/index.js
@@ -1,15 +1,1 @@
-const observer = new IntersectionObserver(observerHandler, {
-    threshold: .5,
-})
 
-function observerHandler(entries) {
-    for (const entry of entries) {
-        if (entry.intersectionRatio > 0) {
-            entry.target.src = entry.target.dataset.src
-        }
-    }
-}
-
-const images = document.querySelectorAll('section img')
-
-images.forEach(img => observer.observe(img))
