f194242525f96980cb8ee91249df277df40e92e9
SerhiiCho <serhiicho@protonmail.com>
2022-12-29 08:26:38 +0200

    WIP with index.js

--- js/intersection-observer/index.js
+++ js/intersection-observer/index.js
@@ -1,7 +1,15 @@
 const images = document.querySelectorAll('section img')
 
-const observer = new IntersectionObserver(observerHandler)
+const observer = new IntersectionObserver(observerHandler, {
+    threshold: 0.5,
+})
 
-function observerHandler(entries, observer) {
-    //
+function observerHandler(entries) {
+    for (const entry of entries) {
+        if (entry.intersectionRatio > 0) {
+            entry.target.src = entry.target.getAttribute('data-src')
+        }
+    }
 }
+
+images.forEach(img => observer.observe(img))
