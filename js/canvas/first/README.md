f39c7e94e37eb759a6abe97b73690549e91be77d
SerhiiCho <serhiicho@protonmail.com>
2023-05-08 22:49:48 +0300

    WIP on canvas

---
+++ js/canvas/first/README.md
@@ -0,0 +1,12 @@
+# Learning Canvas and Improving JavaScript
+
+## Tips
+
+- We don't wont to use global variables inside classes;
+
+## Memorize
+
+###  Incapsulation
+Incapsulating is the bundling data and methods that act on that data in a way
+that access to the data is restricted from outside the bundle, and only the
+methods inside the bundle can access it.
--- js/canvas/first/canvas.js
+++ js/canvas/first/canvas.js
@@ -1,47 +1,21 @@
-const canvas = document.querySelector('canvas')
+window.onload = () => {
+    const canvas = document.getElementById('canvas1')
+    const ctx = canvas.getContext('2d')
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 
-const c = canvas.getContext('2d')
+    const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height)
+}
 
-c.fillStyle = 'rgba(255, 0, 0, 0.5)'
-c.fillRect(100, 100, 100, 100) // x, y, width, height
-c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-c.fillRect(400, 100, 50, 50)
-c.fillStyle = 'rgba(0, 255, 0, 0.5)'
-c.fillRect(300, 300, 170, 80)
+class FlowFieldEffect {
+    #ctx
+    #width
+    #height
 
-// Line
-c.beginPath();
-c.moveTo(10, 10) // x, y // Start of the line
-c.lineTo(300, 10)
-c.lineTo(300, 270)
-c.lineTo(10, 270)
-c.lineTo(10, 10)
-c.strokeStyle = 'brown'
-c.stroke()
-
-// Arc / Circle
-// c, y, radius, startAngle, endAngle, drawCounterClockwise
-const circles = [
-    {
-        x: 300,
-        y: 300
-    },
-    {
-        x: 150,
-        y: 350
-    },
-    {
-        x: 490,
-        y: 300
-    },
-]
-
-circles.forEach(circle => {
-    c.beginPath()
-    c.arc(circle.x, circle.y, 50, 0, Math.PI * 2, false)
-    c.strokeStyle = 'darkgreen'
-    c.stroke()
-});
+    constructor(ctx, width, height) {
+        this.#ctx = ctx
+        this.#width = width
+        this.#height = height
+    }
+}
