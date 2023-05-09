762c375a08a3301fab51887b99e9bd20ab3999b0
SerhiiCho <serhiicho@protonmail.com>
2023-05-09 21:08:30 +0300

    WIP on first game

--- js/canvas/first-game/script.js
+++ js/canvas/first-game/script.js
@@ -9,50 +9,94 @@ const images = {}
 images.player = new Image()
 images.player.src = 'assets/character.png'
 
-const characters = []
-characters.push(new Character())
+const actions = ['up', 'right', 'jump', 'down right']
+const numberOfCharacters = 10
 
 class Character {
-    playerWidth = 103.0625
-    playerHeight = 113.125
-    x = 0
-    y = 0
-    frameX = 3
-    frameY = 3
-    speed
-    action
-
     constructor() {
-        this.speed = (Math.random() * 1.5) + 3.5
-        this.action = 'right'
+        this.width = 103.0625
+        this.height = 113.125
+        this.frameX = 3
+        this.x = Math.random() * (canvas.width - this.width)
+        this.y = Math.random() * (canvas.height - this.height)
+        this.speed = (Math.random() * 5) + 1.5
+        this.action = actions[Math.floor(Math.random() * actions.length)]
+
+        if (this.action === 'right') {
+            this.frameY = 3
+            this.minFrame = 4
+            this.maxFrame = 13
+        } else if (this.action === 'up') {
+            this.frameY = 0
+            this.minFrame = 3
+            this.maxFrame = 13
+        } else if (this.action === 'jump') {
+            this.frameY = 7
+            this.minFrame = 0
+            this.maxFrame = 9
+        } else if (this.action === 'down right') {
+            this.frameY = 4
+            this.minFrame = 4
+            this.maxFrame = 15
+        }
     }
 
     draw() {
-        const sX = this.playerWidth * this.frameX
-        const sY = this.playerHeight * this.frameY
+        const sX = this.width * this.frameX
+        const sY = this.height * this.frameY
 
         drawSprite(images.player, sX, sY, this.width, this.height, this.x, this.y, this.width, this.height)
 
-        if (this.frameX < 13) {
+        if (this.frameX < this.maxFrame) {
             this.frameX++
         } else {
-            this.frameX = 3
+            this.frameX = this.minFrame
         }
     }
 
     update() {
         if (this.action === 'right') {
             this.#runRight()
+        } else if (this.action === 'up') {
+            this.#runUp()
+        } else if (this.action === 'down right') {
+            this.#runDownRight()
         }
     }
 
     #runRight() {
-        if (this.x < canvas.width - this.width) {
+        if (this.x < canvas.width + this.width) {
             this.x += this.speed
         } else {
             this.x = 0 - this.width
+            this.y = Math.random() * (canvas.height - this.height)
         }
     }
+
+    #runUp() {
+        if (this.y > 0 - this.height) {
+            this.y -= this.speed
+        } else {
+            this.y = canvas.height + this.height
+            this.x = Math.random() * (canvas.width - this.width)
+        }
+    }
+
+    #runDownRight() {
+        if (this.y > canvas.height + this.height && this.x > this.width + canvas.width) {
+            this.x = 0 - this.height
+            this.y = Math.random() * (canvas.width / 2)
+        } else {
+            this.x += this.speed
+            this.y += this.speed
+        }
+    }
+}
+
+const characters = []
+
+for (let i = 0; i < numberOfCharacters; i++) {
+    characters.push(new Character())
 }
 
 /**
@@ -79,7 +123,7 @@ function animate() {
     })
 }
 
-window.onload = setInterval(animate, 1000 / 20)
+window.onload = setInterval(animate, 1000 / 30)
 
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth
