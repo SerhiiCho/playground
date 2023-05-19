91aaaca3292c8adb06baa3c2bbe44d0720794f12
SerhiiCho <serhiicho@protonmail.com>
2023-05-19 21:22:52 +0300

    Added animation for game title

--- ts/phaser/Collect Diamons/src/Models/StartButton.ts
+++ ts/phaser/Collect Diamons/src/Models/StartButton.ts
@@ -1,3 +1,5 @@
+import Phaser from 'phaser'
+
 export default class {
     constructor(
         public sprite: Phaser.GameObjects.Sprite,
--- ts/phaser/Collect Diamons/src/Scenes/StartScene.ts
+++ ts/phaser/Collect Diamons/src/Scenes/StartScene.ts
@@ -3,12 +3,16 @@ import { sceneConfig } from '@/config'
 import imageBackground from '@/assets/start.jpg'
 import startGameImage from '@/assets/start-game-button.png'
 import startGameImageHover from '@/assets/start-game-button-hover.png'
+import mainTitle from '@/assets/title.png'
 import StartButton from '@/Models/StartButton'
+import MainTitle from '@/Models/MainTitle'
+import Position from '@/Models/Position'
 
 export default class StartScene extends Phaser.Scene {
     private canvasWidth: number = 0
     private canvasHeight: number = 0
     private startButton: StartButton | undefined
+    private mainTitle: MainTitle | undefined
 
     constructor() {
         super(sceneConfig.start)
@@ -18,6 +22,7 @@ export default class StartScene extends Phaser.Scene {
         this.load.image('start-background', imageBackground)
         this.load.image('start-game', startGameImage)
         this.load.image('start-game-hover', startGameImageHover)
+        this.load.image('main-title', mainTitle)
 
         this.canvasWidth = Number(this.sys.game.config.width)
         this.canvasHeight = Number(this.sys.game.config.height)
@@ -27,6 +32,9 @@ export default class StartScene extends Phaser.Scene {
         this.add
             .sprite(0, 0, 'start-background')
             .setPosition(this.canvasWidth / 2, this.canvasHeight / 2)
+
+        this.mainTitle = new MainTitle(this.add.sprite(0, 0, 'main-title'))
+        this.mainTitle.create()
 
         this.startButton = new StartButton(
             this.drawButton('start-game'),
@@ -38,6 +46,10 @@ export default class StartScene extends Phaser.Scene {
         this.startButton.handleButtonClick(() => {
             this.scene.start(sceneConfig.play.key)
         })
+    }
+
+    public update(): void {
+        this.mainTitle!.update()
     }
 
     private drawButton(button: string): Phaser.GameObjects.Sprite {
