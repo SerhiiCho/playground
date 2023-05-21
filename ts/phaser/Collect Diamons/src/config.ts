398bb3992e4b48115a323b22819b7bf4c02ae8fe
SerhiiCho <serhiicho@protonmail.com>
2023-05-21 07:49:53 +0300

    Played with Phaser config

--- ts/phaser/Collect Diamons/src/config.ts
+++ ts/phaser/Collect Diamons/src/config.ts
@@ -3,22 +3,33 @@ import PlayScene from '@/Scenes/PlayScene'
 import StartScene from '@/Scenes/StartScene'
 import GameOverScene from '@/Scenes/GameOverScene'
 
-export const gameConfig = {
-  type: Phaser.AUTO,
-  width: 640,
-  height: 360,
-  title: 'Collect Diamonds',
-  scene: [StartScene, PlayScene, GameOverScene],
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    version: '1.0',
+    banner: false,
+    title: 'Collect Diamonds',
+    scene: [StartScene, PlayScene, GameOverScene],
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: import.meta.env.DEV,
+        },
+    },
 }
 
 export const sceneConfig = {
-  start: {
-    key: 'start',
-  },
-  play: {
-    key: 'play',
-  },
-  gameOver: {
-    key: 'gameOver',
-  },
+    start: {
+        key: 'start',
+    },
+    play: {
+        key: 'play',
+    },
+    gameOver: {
+        key: 'gameOver',
+    },
 }
