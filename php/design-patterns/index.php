91f33ae232a0c2c70b29d41e8b90bf36c42566f2
SerhiiCho <serhiicho@protonmail.com>
2023-06-27 08:21:19 +0300

    WIP on abstract factory example

--- php/design-patterns/index.php
+++ php/design-patterns/index.php
@@ -1,2 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
+use App\AbstractFactory\Manufacturer\MsiManufacturer;
+use App\AbstractFactory\Manufacturer\AsusManufacturer;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+function abstractFactory(): void {
+    echo "<-- Testing Abstract Factory pattern... -->\n";
+
+    $msi = new MsiManufacturer();
+
+    $msiGpu = $msi->createGpu();
+    $msiMonitor = $msi->createMonitor();
+
+    $msiGpu->assemble();
+    $msiMonitor->assemble();
+
+    $asus = new AsusManufacturer();
+
+    $asusGpu = $asus->createGpu();
+    $asusMonitor = $asus->createMonitor();
+
+    $asusGpu->assemble();
+    $asusMonitor->assemble();
+
+    echo "<-- Abstract Factory pattern test done! -->\n";
+}
+
+abstractFactory();
--- php/design-patterns/src/AbstractFactory/Manufacturer/MsiManufacturer.php
+++ php/design-patterns/src/AbstractFactory/Manufacturer/MsiManufacturer.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\AbstractFactory\Manufacturer;
 
 use App\AbstractFactory\Products\Gpu\Gpu;
-use App\AbstractFactory\Products\Monitor\Monitor;
 use App\AbstractFactory\Products\Gpu\MsiGpu;
+use App\AbstractFactory\Manufacturer\Company;
+use App\AbstractFactory\Products\Monitor\Monitor;
 use App\AbstractFactory\Products\Monitor\MsiMonitor;
 
 class MsiManufacturer extends Company
