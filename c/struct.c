5ea6249574453d5f1a7ee002a928693bf9a81202
SerhiiCho <serhiicho@protonmail.com>
2023-06-29 15:15:39 +0300

    Changed C example

--- c/struct.c
+++ c/struct.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 struct User
 {
@@ -7,18 +8,28 @@ struct User
     int age;
 };
 
-int main()
+void structExample()
 {
     struct User firstUser;
     firstUser.age = 29;
     strcpy(firstUser.name, "Mikel");
 
+    printf("structExample: %s\n", firstUser.name);
+}
+
+void nestedArrayExample()
+{
     int stuff[2][3] = {
         {1, 2, 3},
         {5, 4, 2}
     };
 
-    printf("%s", firstUser.name);
+    printf("nestedArrayExample: %d\n", stuff[0][0]);
+}
 
+int main()
+{
+    structExample();
+    nestedArrayExample();
     return 0;
 }
