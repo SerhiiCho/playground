d5b57884281081d36bf188af70338425b8afdf8c
SerhiiCho <serhiicho@protonmail.com>
2023-07-09 09:53:43 +0300

    Implemented bubble sort in golang

---
+++ go/data_structures/bubblesort.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{24, 7, 6, 2342, 4, 5, 235, 3, 52324, 2, 0}
+
+	res := sort(nums, 0)
+
+	fmt.Println(res)
+}
+
+func sort(nums []int, exclude int) []int {
+	length := len(nums)
+
+	if exclude >= length {
+		return nums
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if i+1 >= length {
+			continue
+		}
+
+		next := nums[i+1]
+		curr := nums[i]
+
+		if curr > next {
+			nums[i+1] = curr
+			nums[i] = next
+		}
+	}
+
+	return sort(nums, exclude+1)
+}
