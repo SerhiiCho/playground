90a1bd07774228448d15203a19da354c994d4fa3
SerhiiCho <serhiicho@protonmail.com>
2023-07-15 21:15:24 +0300

    Playing around with haskell

---
+++ haskell/hello.hs
@@ -0,0 +1,7 @@
+main = do
+    let name = "Serhii"
+    let numOfGifts = 36
+
+    -- This line prints Hello
+    putStrLn ("Hello " ++ name ++ "!")
+    putStrLn ("He has " ++ show numOfGifts ++ " gifts!")
