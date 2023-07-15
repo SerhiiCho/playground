d814818cd6ab8ed5f320a7b9a50eccbc90b3feb2
SerhiiCho <serhiicho@protonmail.com>
2023-07-15 21:27:12 +0300

    Types in haskell

---
+++ haskell/types.hs
@@ -0,0 +1,17 @@
+name :: String
+name = "Serhii"
+
+myLetter :: Char
+myLetter = 's'
+
+numOfGifts :: Int
+numOfGifts = 36
+
+gpa :: Double
+gpa = 3.6
+
+main = do
+    putStrLn name
+    print numOfGifts
+    print myLetter
+    print gpa
