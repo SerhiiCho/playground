e6acde49f50fa41676ee58deb410e631f05b0ebd
SerhiiCho <serhiicho@protonmail.com>
2023-07-16 13:33:04 +0300

    Added calc.hs

---
+++ haskell/calc.hs
@@ -0,0 +1,12 @@
+main = do
+    putStrLn "Enter first number: "
+    firstNumber <- getLine
+
+    putStrLn "Enter second number: "
+    secondNumber <- getLine
+
+    let firstNum = read firstNumber :: Int
+    let secondNum = read secondNumber :: Int
+    let sum = firstNum + secondNum
+
+    putStrLn (firstNumber ++ " + " ++ secondNumber ++ " = " ++ (show sum))
