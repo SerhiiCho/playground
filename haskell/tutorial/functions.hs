25188a2bd7506aa6cf82a9d11476b3f82d3effc9
SerhiiCho <serhiicho@protonmail.com>
2023-07-16 14:19:49 +0300

    Finished with function.hs example

--- haskell/tutorial/functions.hs
+++ haskell/tutorial/functions.hs
@@ -1,6 +1,17 @@
 sayHi :: String -> Int -> IO ()
 sayHi name age = putStrLn ("Hello " ++ name ++ "! You are " ++ show age ++ " years old!")
 
+cube :: Int -> Int
+cube num = num * num * num
+
+createFullName :: String -> String -> String
+createFullName first last = first ++ " " ++ last
+
 main :: IO ()
 main = do
     sayHi "Anna" 25
+    putStrLn (show (cube 3))
+
+    let fullName = createFullName "Anna" "Korotchaeva"
+
+    putStrLn fullName
