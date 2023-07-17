ef995f943c63f03132bc89f2b027d1abca29bc1d
SerhiiCho <serhiicho@protonmail.com>
2023-07-17 07:58:56 +0300

    Added haskell example of case condition

--- haskell/tutorial/conditionals.hs
+++ haskell/tutorial/conditionals.hs
@@ -7,6 +7,17 @@ travelToWork weather = do
             then putStrLn "Biking to work"
         else putStrLn "Driving to work"
 
+
+gradeMeaning :: String -> IO ()
+gradeMeaning grade = do
+    case grade of
+        "A" -> putStrLn "Great work!"
+        "B" -> putStrLn "Pretty good!"
+        "C" -> putStrLn "You did alright"
+        "D" -> putStrLn "Not so great"
+        _   -> putStrLn "Sorry, you failed!"
+
 main :: IO ()
 main = do
     travelToWork "cloudy"
+    gradeMeaning "B"
