e29c9239c44038c699f5b78f2c06c8b5f02cfba0
SerhiiCho <serhiicho@protonmail.com>
2023-07-18 14:58:49 +0300

    WIP on guessingGame.hs

---
+++ haskell/tutorial/guessingGame.hs
@@ -0,0 +1,23 @@
+maxGuesses :: Int
+maxGuesses = 3
+
+runGame :: Int -> IO ()
+runGame guesses = do
+    let secretNumber = "5"
+    let userGuess = ""
+
+    if guesses == maxGuesses
+        then putStrLn ("Sorry! You exceeded all guesses!")
+        else do
+            putStrLn "Enter a guess number between 1 and 10: "
+            userGuess <- getLine
+
+            if userGuess == secretNumber
+                then putStrLn "You Win!"
+                else do
+                    putStrLn ("You Lost! The number was: " ++ secretNumber)
+                    runGame (guesses + 1)
+
+main :: IO ()
+main = do
+    runGame 0
