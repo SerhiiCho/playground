8c7b32cc74203d564d3c5c6d142f3365e78b6cab
SerhiiCho <serhiicho@protonmail.com>
2023-07-19 09:22:06 +0300

    Added more examples for functions.hs

--- haskell/tutorial/functions.hs
+++ haskell/tutorial/functions.hs
@@ -1,11 +1,21 @@
+-- Function that prints out a string
 sayHi :: String -> Int -> IO ()
 sayHi name age = putStrLn ("Hello " ++ name ++ "! You are " ++ show age ++ " years old!")
 
+-- Inline function with one argument
 cube :: Int -> Int
 cube num = num * num * num
 
+-- Inline function with multiple arguments
 createFullName :: String -> String -> String
 createFullName first last = first ++ " " ++ last
+
+-- Pattern matching function
+yourNameIs :: String -> String
+yourNameIs name
+  | name == "Serhii" = "You like Anna"
+  | name == "Anna" = "You like Serhii"
+  | otherwise = "You like someone else"
 
 main :: IO ()
 main = do
@@ -15,3 +25,4 @@ main = do
     let fullName = createFullName "Anna" "Korotchaeva"
 
     putStrLn fullName
+    putStrLn (yourNameIs "Serhii")
