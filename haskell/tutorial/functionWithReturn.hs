c718e2ad54f30c8defd30687ba83be044209b071
SerhiiCho <serhiicho@protonmail.com>
2023-07-19 22:01:31 +0300

    Added some explanation to function with return keyword

--- haskell/tutorial/functionWithReturn.hs
+++ haskell/tutorial/functionWithReturn.hs
@@ -1,3 +1,4 @@
+-- In this example we use the return keyword
 getName :: IO String
 getName = do
     putStrLn "What is your name?"
@@ -6,3 +7,12 @@ getName = do
 
 main :: IO ()
 main = getName >>= \n -> putStrLn ("Hello " ++ n ++ "!")
+
+{-
+    The `\` symbol before the variable `n` is called a lambda.
+    It signifies that we are defining an anonymous function.
+    It is a way to define an anonymous function.
+
+    The `>>=` operator is called bind. It is used to bind
+    the result of an IO action to a variable.
+-}
