cd90d410701638713aee5135d0e4402fa1acf741
SerhiiCho <serhiicho@protonmail.com>
2023-07-23 13:36:25 +0300

    Added when.hs example

---
+++ haskell/tutorial/when.hs
@@ -0,0 +1,14 @@
+import Control.Monad (when)
+
+printWhenEqual :: Int -> Int -> IO ()
+printWhenEqual num1 num2 = when (num1 == num2) $ putStrLn "Numbers are the same"
+
+printWhenDifferentStrings :: String -> String -> IO ()
+printWhenDifferentStrings str1 str2
+    | str1 == str2 = putStrLn "Strings are the same"
+    | otherwise = putStrLn "Strings are not the same"
+
+main :: IO ()
+main = do
+    printWhenEqual 24 23
+    printWhenDifferentStrings "nice" "cool"
