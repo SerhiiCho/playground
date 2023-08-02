993f73203aa0ec12d82dc43f767ab44d63ec5b56
SerhiiCho <serhiicho@protonmail.com>
2023-08-02 10:57:53 +0300

    Added more to a tuple example

--- haskell/tuples.hs
+++ haskell/tuples.hs
@@ -1,4 +1,10 @@
 main :: IO ()
 main = do
+    -- Tuple can have as many values as you want
     let myTuple = (33, "Serhii") :: (Int, String)
-    print myTuple
+
+    print ("My name is " ++ snd myTuple)
+
+    let inTenYears = (fst myTuple) + 10
+
+    print ("In 10 years I will be " ++ (show inTenYears))
