fb87e1560d76a5a5c9a34bad4349ea645da1535b
SerhiiCho <serhiicho@protonmail.com>
2023-08-24 10:50:57 +0300

    Moved 2 dirs

---
+++ frameworks/laravel/blade/views/nav.blade.php
@@ -0,0 +1,7 @@
+<nav style="margin-bottom: 20px">
+    <div class="nav-wrapper red darken-3">
+        <div class="container">
+            <a href="#" class="brand-logo">Blade example</a>
+        </div>
+    </div>
+</nav>
