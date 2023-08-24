fa00ee6368bd6ddb4975a7b02c48caab2bc0ec0a
SerhiiCho <serhiicho@protonmail.com>
2023-08-24 10:54:19 +0300

    Deleted files

--- languages/php/blade/cache/bd015f80158a1ba099b72ee0afb41acbce5ab395.php
+++
@@ -1,7 +0,0 @@
-<nav style="margin-bottom: 20px">
-    <div class="nav-wrapper red darken-3">
-        <div class="container">
-            <a href="#" class="brand-logo">Blade example</a>
-        </div>
-    </div>
-</nav><?php /**PATH /home/serhii/www/pg/php/Blade/views/nav.blade.php ENDPATH**/ ?>
--- languages/php/blade/views/nav.blade.php
+++
@@ -1,7 +0,0 @@
-<nav style="margin-bottom: 20px">
-    <div class="nav-wrapper red darken-3">
-        <div class="container">
-            <a href="#" class="brand-logo">Blade example</a>
-        </div>
-    </div>
-</nav>
