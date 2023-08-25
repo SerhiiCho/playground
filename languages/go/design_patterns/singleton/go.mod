d7434a88200b5502b39a1dc3072a415bee17084b
SerhiiCho <serhiicho@protonmail.com>
2023-08-25 20:30:11 +0300

    Added singleton example for golang

--- languages/go/design_patterns/singleton/go.mod
+++
@@ -1,3 +0,0 @@
-module singleton
-
-go 1.21.0
--- languages/go/design_patterns/singleton/main.go
+++ languages/go/design_patterns/singleton/main.go
@@ -1,5 +1,42 @@
 package main
 
+import "fmt"
+
 func main() {
-	// here
+	log := getLoggerInstance()
+
+	log.SetLogLevel(1)
+	log.Log("This is a log message")
+
+	log = getLoggerInstance()
+	log.SetLogLevel(2)
+	log.Log("This is a log message")
+
+	log = getLoggerInstance()
+	log.SetLogLevel(3)
+	log.Log("This is a log message")
+}
+
+type MyLogger struct {
+	loglevel int
+}
+
+func (l *MyLogger) Log(message string) {
+	fmt.Println(l.loglevel, ":", message)
+}
+
+func (l *MyLogger) SetLogLevel(level int) {
+	l.loglevel = level
+}
+
+var logger *MyLogger
+
+func getLoggerInstance() *MyLogger {
+	if logger == nil {
+		fmt.Println("Creating logger instance")
+		logger = &MyLogger{}
+	}
+
+	fmt.Println("Returning logger instance")
+	return logger
 }
