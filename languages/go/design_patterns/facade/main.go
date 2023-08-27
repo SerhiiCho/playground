ffa46ca163b91046e549473e3b1392443c731bbb
SerhiiCho <serhiicho@protonmail.com>
2023-08-27 12:14:24 +0300

    Added Go example of facade pattern

---
+++ languages/go/design_patterns/facade/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	. "facade/app"
+)
+
+func main() {
+	// Use the Facade Cafe API to create coffee drinks
+	// instead of directly interacting with the complex Coffee API
+
+	// Make an 8 ounce Americano
+	MakeAmericano(8.0)
+
+	// Make a 12 ounce Latte
+	MakeLatte(12.0, true)
+}
