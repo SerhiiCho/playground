08a7e0ccc2899acc76d06e286ec0ff0c8b7fb596
SerhiiCho <serhiicho@protonmail.com>
2023-08-27 12:17:39 +0300

    Added starting template for go observer pattern

---
+++ languages/go/design_patterns/observer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import . "observer/app"
+
+func main() {
+	// Construct two DataListener observers and
+	// give each one a name
+	listener1 := DataListener{
+		Name: "Listener 1",
+	}
+	listener2 := DataListener{
+		Name: "Listener 2",
+	}
+
+	// Create the DataSubject that the listeners will observe
+	subj := &DataSubject{}
+	// TODO: Register each listener with the DataSubject
+
+	// TODO: Change the data in the DataSubject - this will cause the
+	// onUpdate method of each listener to be called
+
+	// TODO: Try to unregister one of the observers
+}
