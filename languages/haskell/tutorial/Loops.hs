60d451b60cec6c1ff09d42eda8c871221b3995f6
SerhiiCho <serhiicho@protonmail.com>
2023-08-28 21:52:24 +0300

    Changes to Haskell tutorial files

--- languages/haskell/tutorial/Loops.hs
+++ languages/haskell/tutorial/Loops.hs
@@ -8,6 +8,23 @@ printTo100 num = do
             printTo100 (num + 1)
         else putStrLn("Completed the loop")
 
+count :: Int -> [Int] -> Int
+count n list = go 0 list
+    where
+        go :: Int -> [Int] -> Int
+        go acc [] = acc
+        go acc (x : xs)
+            | x == n = go (acc + 1) xs
+            | otherwise = go acc xs
+
+
+listHasTwoElements :: [Int] -> Bool
+listHasTwoElements list = case list of
+    [_, _] -> True
+    _ -> False
+
 main :: IO ()
 main = do
     printTo100 1
+    print (count 2 [2, 4, 5, 6, 8, 9, 2, 4, 9, 2]) -- returns 3
+    print (listHasTwoElements [2, 3]) -- returns True
--- languages/haskell/tutorial/PatternMatching.hs
+++ languages/haskell/tutorial/PatternMatching.hs
@@ -25,10 +25,21 @@ oneOrTwoZeros l = case l of
     [0, 0] -> True
     _ -> False
 
+headOrDef :: Int -> [Int] -> Int
+headOrDef def [] = def
+headOrDef _ (x : _) = x -- pattern matching to first element of the list (x : _)
+
+-- Function sum of the number in the list with pattern matching
+getSum :: [Int] -> Int
+getSum [] = 0
+getSum (x : xs) = x + getSum xs
+
 main :: IO ()
 main = do
     print (getName "Anna") -- returns "Awesome An"
     print (getAge 18) -- returns "Wow! You are already more than 18 years old"
     print (sumOfTwoInThree [1, 2, 3]) -- returns 4
     print (oneOrTwoZeros [0, 0]) -- returns True
+    print (headOrDef 38 [1, 2, 3, 4, 5]) -- returns 1
+    print (getSum [25, 25, 50, 100, 300]) -- returns 500
 
