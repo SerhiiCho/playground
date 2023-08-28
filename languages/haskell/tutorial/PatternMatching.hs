877d170587d9d2de59a2f118d21899d577fcc309
SerhiiCho <serhiicho@protonmail.com>
2023-08-28 17:31:10 +0300

    Added examples to pattern matching in haskell

--- languages/haskell/tutorial/PatternMatching.hs
+++ languages/haskell/tutorial/PatternMatching.hs
@@ -13,8 +13,22 @@ getAge 1 = "Only 1 year old"
 getAge 2 = "Only 2 years old"
 getAge age = "Wow! You are already more than " ++ show age ++ " years old"
 
+-- We can do something even crazier with lists
+-- If the list has exactly 3 items, we want to get the sum of first and last
+sumOfTwoInThree :: [Int] -> Int
+sumOfTwoInThree [x, _, y] = x + y
+sumOfTwoInThree _ = 0
+
+oneOrTwoZeros :: [Int] -> Bool
+oneOrTwoZeros l = case l of
+    [0] -> True
+    [0, 0] -> True
+    _ -> False
+
 main :: IO ()
 main = do
-    print (getName "Anna")
-    print (getAge 18)
+    print (getName "Anna") -- returns "Awesome An"
+    print (getAge 18) -- returns "Wow! You are already more than 18 years old"
+    print (sumOfTwoInThree [1, 2, 3]) -- returns 4
+    print (oneOrTwoZeros [0, 0]) -- returns True
 
