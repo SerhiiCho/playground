6eb845e6e64e6f75b1d3222b0f3c43207a545d9b
SerhiiCho <serhiicho@protonmail.com>
2023-09-03 21:17:09 +0300

    Modified basic.py file

--- languages/python/traversy/basic.py
+++ languages/python/traversy/basic.py
@@ -4,16 +4,16 @@ Doc string is looking like this
 
 # This is also comments
 
-#x = 1 # int
-#y = 2.5 # float
-#name = 'John' # str
-#is_cool = True # bool
+x = 1 # int
+y = 2.5 # float
+name = 'John' # str
+is_cool = True # bool
 
 # Does the same as above
 x, y, name, is_cool = (1, 2.5, 'John', True)
 
 # To check the type
-# type(x)
+print(type(x))
 
 # To convert the type
 # str(x) int(x) float(x) bool(x)
