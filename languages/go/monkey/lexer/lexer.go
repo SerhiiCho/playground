f64c1b64d735219c1d9dffaa6e5899c06038239f
SerhiiCho <serhiicho@protonmail.com>
2023-09-11 08:32:38 +0300

    WIP on monkey programming language based on book

---
+++ languages/go/monkey/lexer/lexer.go
@@ -0,0 +1,122 @@
+package lexer
+
+import (
+	"monkey/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // points to position of the char
+	nextPosition int  // points to the next char in the input
+	char         byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.advanceChar()
+
+	return l
+}
+
+// advanceChar gives us the next character and advances
+// our position in the input string
+func (l *Lexer) advanceChar() {
+	if l.nextPosition >= len(l.input) {
+		l.char = 0
+	} else {
+		l.char = l.input[l.nextPosition]
+	}
+
+	l.position = l.nextPosition
+	l.nextPosition += 1
+}
+
+// NextToken looks at current char and returns a token depending
+// on which character it is
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.char {
+	case '=':
+		tok = newToken(token.ASSIGN, l.char)
+	case ';':
+		tok = newToken(token.SEMI, l.char)
+	case '(':
+		tok = newToken(token.LPAREN, l.char)
+	case ')':
+		tok = newToken(token.RPAREN, l.char)
+	case ',':
+		tok = newToken(token.COMMA, l.char)
+	case '+':
+		tok = newToken(token.PLUS, l.char)
+	case '{':
+		tok = newToken(token.LBRACE, l.char)
+	case '}':
+		tok = newToken(token.RBRACE, l.char)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.char) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}
+
+		if isDigit(l.char) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		}
+
+		tok = newToken(token.ILLEGAL, l.char)
+	}
+
+	l.advanceChar()
+
+	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.char == ' ' || l.char == '\t' || l.char == '\n' || l.char == '\r' {
+		l.advanceChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+
+	for isLetter(l.char) {
+		l.advanceChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.char) {
+		l.advanceChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isDigit(char byte) bool {
+	return '0' <= char && char <= '9'
+}
+
+func isLetter(char byte) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+// newToken creates a token based on the provided data
+func newToken(tokenType token.TokenType, char byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(char),
+	}
+}
---
+++ languages/go/monkey/token/token.go
@@ -0,0 +1,47 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL" // Signifies a token/character we don't know about
+	EOF     = "EOF"     // stands for End Of File
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA  = ","
+	SEMI   = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNC = "FUNCTION"
+	LET  = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNC,
+	"let": LET,
+}
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// LookupIdent checks if given identifier is a keyword
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT // all other user-defined identifiers
+}
