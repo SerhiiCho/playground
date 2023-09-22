b5d5f1e768ab1f0d14d7698f0cc90a4c91dde9dd
SerhiiCho <serhiicho@protonmail.com>
2023-09-22 19:16:44 +0300

    monkey:wip on evaluation with - prefix

--- languages/go/monkey/evaluator/evaluator.go
+++ languages/go/monkey/evaluator/evaluator.go
@@ -5,36 +5,67 @@ import (
 	"monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
 	// Statements
 	case *ast.Program:
 		return evalStatements(node.Statements)
-
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
-	}
-
-	program, isProgram := node.(*ast.Program)
-
-	if isProgram {
-		lastSt := len(program.Statements) - 1
-
-		for i, st := range program.Statements {
-			evaluated := Eval(st)
-
-			if i == lastSt {
-				return evaluated
-			}
-		}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	//
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalStatements(statements []ast.Statement) object.Object {
--- languages/go/monkey/evaluator/evaluator_test.go
+++ languages/go/monkey/evaluator/evaluator_test.go
@@ -14,11 +14,47 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-2", -2},
+		{"-11", -11},
 	}
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
 
@@ -40,6 +76,22 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	if result.Value != expected {
 		t.Errorf("result.Value must be %d, got=%d", expected, result.Value)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("object is not Boolean. Got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("result.Value must be %t, got=%t", expected, result.Value)
 		return false
 	}
 
