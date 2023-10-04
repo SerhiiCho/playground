763a8c79e823fdf100948c7ae8a1fa7a4a9028ae
SerhiiCho <serhiicho@protonmail.com>
2023-10-04 09:53:33 +0300

    monkey:added support for functions

--- languages/go/monkey/evaluator/evaluator.go
+++ languages/go/monkey/evaluator/evaluator.go
@@ -88,9 +88,42 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if len(args) == 1 && isError(args[0]) {
 			return args[0]
 		}
+
+		return applyFunction(function, args)
 	}
 
 	return nil
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for key, param := range fn.Parameters {
+		env.Set(param.Value, args[key])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
