40f08cce52e21d7bd95ea5fbc4c77e925f126953
SerhiiCho <serhiicho@protonmail.com>
2023-12-28 11:32:27 +0200

    WIP on textwire example usage

--- languages/go/textwire_example/main.go
+++ languages/go/textwire_example/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	. "github.com/textwire/textwire"
+	"github.com/textwire/textwire"
 )
 
 func main() {
-	res, err := ParseText("<h2>{{ title }}</h2>", map[string]interface{}{
-		"title": "Hello World",
-	})
+	http.HandleFunc("/", home)
+	fmt.Println("Listening on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	vars := map[string]interface{}{
+		"title": "Hello, World!",
+		"age":   23,
+	}
+
+	// res, err := textwire.ParseFile("templates/home", vars)
+	res, err := textwire.ParseText("<h1>{{ title }} and {{ age }}</h1>", vars)
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(res)
+	fmt.Fprintf(w, res)
 }
