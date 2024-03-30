78e3c143870bfa78174dc9f68d4aeba837638d16
SerhiiCho <serhiicho@protonmail.com>
2024-03-30 19:05:25 +0200

    Created Castle Fight test project

---
+++ languages/typescript/phaser/Castle Fight/src/config.ts
@@ -0,0 +1,11 @@
+import Phaser from 'phaser'
+
+export const gameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    version: '0.0.1',
+    banner: false,
+    title: 'Castle Fight',
+    scene: [],
+}
---
+++ languages/typescript/phaser/Castle Fight/src/main.ts
@@ -0,0 +1,5 @@
+import './style.css'
+import { gameConfig } from '@/config'
+import Phaser from 'phaser'
+
+new Phaser.Game(gameConfig)
