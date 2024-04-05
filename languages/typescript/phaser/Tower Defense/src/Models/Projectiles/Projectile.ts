d158e1e29dec2adb63f4e4af1f60c0843e199af0
SerhiiCho <serhiicho@protonmail.com>
2024-04-05 09:51:27 +0300

    Fix issue when arrow is out of bound

--- languages/typescript/phaser/Tower Defense/src/Models/Projectiles/Projectile.ts
+++ languages/typescript/phaser/Tower Defense/src/Models/Projectiles/Projectile.ts
@@ -3,7 +3,7 @@ import Enemy from '@/Models/Enemy/Enemy'
 import Tower from '@/Models/Tower/Tower'
 
 export default class Projectile {
-    private isShooting: boolean = false
+    public isShooting: boolean = false
     private lastShotTime: number = 0
 
     public constructor(
@@ -30,11 +30,17 @@ export default class Projectile {
         const timeDiff = currentTime - this.lastShotTime
         const canShoot = timeDiff >= this.shotDelay
 
+        this.alignProjectile(enemy)
+
+        if (this.isOutOfBound()) {
+            this.isShooting = false
+            this.image.setVisible(false)
+        }
+
         if (this.isShooting || !enemy.isAlive() || !canShoot) {
             return
         }
 
-        this.alignProjectile(enemy)
         this.image.scene.sound.play(this.flySound, { volume: 0.1 })
         this.lastShotTime = currentTime
 
@@ -49,15 +55,28 @@ export default class Projectile {
             }
 
             this.image.scene.sound.play(this.hitSound, { volume: 1.1 })
-            this.image.setVisible(false)
             this.isShooting = false
+            this.image.setVisible(false)
+
+            // stop the projectile
+            this.image.scene.physics.moveToObject(this.image, enemy.sprite, 0)
 
             enemy.hitEnemy(this.damage)
+
             overlap.destroy()
         })
     }
 
+    private isOutOfBound(): boolean {
+        const { width, height } = this.image.scene.game.canvas
+        return this.image.y < 0 || this.image.y > height || this.image.x < 0 || this.image.x > width
+    }
+
     private alignProjectile(enemy: Enemy): void {
+        if (this.isOutOfBound()) {
+            return
+        }
+
         const angle = Phaser.Math.Angle.Between(this.image.x, this.image.y, enemy.sprite.x, enemy.sprite.y)
         this.image.setRotation(angle + 44.7)
     }
