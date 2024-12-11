733508ae13c438001a38fba63c443b0c74528b78
SerhiiCho <serhiicho@protonmail.com>
2024-12-11 20:28:57 +0200

    Upgrade

--- languages/php/ago-example/index.php
+++ languages/php/ago-example/index.php
@@ -2,11 +2,23 @@
 
 declare(strict_types=1);
 
+use Serhii\Ago\LangOverwrite;
+use Serhii\Ago\LangForm;
 use Serhii\Ago\TimeAgo;
+use Serhii\Ago\Config;
+use Serhii\Ago\Lang;
 
 require_once 'vendor/autoload.php';
 
-$toNewYear = strtotime('midnight 1 January next year');
-$output = TimeAgo::trans($toNewYear);
+$config = new Config(overwrites: [
+    new LangOverwrite(
+        lang: Lang::EN,
+        ago: 'before',
+        second: new LangForm(one: 's', other: 's'),
+    ),
+]);
 
-echo $output; // Outputs time left to New Year
+TimeAgo::configure($config);
+
+$date = '-10 seconds';
+echo TimeAgo::trans($date); // "10 s before"
