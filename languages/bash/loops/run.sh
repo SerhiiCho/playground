00d0d4a9129ee4e5a1ecf2809c8b251de37345ca
SerhiiCho <serhiicho@protonmail.com>
2025-08-11 09:37:08 +0300

    chore: update run.sh for bash language

--- languages/bash/loops/run.sh
+++ languages/bash/loops/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
-for NAME in $(cat list.txt); do
-    echo "My name is ${NAME^^}"
+# ${var^^} to uppercase (alice -> ALICE)
+# ${var,,} to lowercase (ALICE → alice)
+# ${var~~} toogle case (Alice → aLICE)
+# ${var^} capitalize (alice → Alice)
+#
+for name in $(cat list.txt); do
+    echo "My name is '${name^}'"
 done
