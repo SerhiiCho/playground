29ed3d0f016e1b1820684effa9edca64c9550ee4
SerhiiCho <serhiicho@protonmail.com>
2025-08-18 16:49:16 +0300

    chore: test.py chagne

--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
-def efun(num):
+def efun(num: int) -> int:
     if num == 0:
         print('done')
         return 1
     else:
         print(num)
         return num * efun(num - 2)
+
 
 efun(8)
